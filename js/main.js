42ef5806156660df895e49f7b6309c00ea6ba989
Megan McKeough <megan.mckeough@gmail.com>
2018-06-16 17:48:17 +1000

    unit 8 game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+cardOne = cards[0];
+cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
