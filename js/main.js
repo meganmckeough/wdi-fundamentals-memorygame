5a46c6af368b7a7deae3f90b1889fb69f548af5e
Megan McKeough <megan.mckeough@gmail.com>
2018-06-17 12:12:27 +1000

    unit 9 created game logic func

--- js/main.js
+++ js/main.js
@@ -4,19 +4,22 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-cardOne = cards[0];
-cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+var flipCards = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCards(0);
+flipCards(2);
 
